0fd4b79b695859fa97708763b18487a972a196e9
nagasawa <nothan87@gmail.com>
2021-12-08 15:16:06 +0900

    fixed start-ubuntu.sh is run only in termux

--- start-ubuntu.sh
+++ start-ubuntu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -v TERMUX_VERSION ]; then
+  echo "run only in termux"
+  exit 1
+fi
+
 options="--termux-home --shared-tmp --bind ${HOME}/opt:/opt --bind ${HOME}:/root"
 
 proot-distro login ubuntu $options "$@"
