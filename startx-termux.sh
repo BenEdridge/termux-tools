8e65de854f269fe065c6c69a4e552343203f739e
nagasawa <nothan87@gmail.com>
2021-12-08 14:27:05 +0900

    updated startx-termu.sh supported .xinitrc in start-ubuntu.sh

--- startx-termux.sh
+++ startx-termux.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 termux-x11 :0 &
 
 if [ -e ~/.xinitrc ]; then
   sleep 1
-  . ~/.xinitrc &
+  if [ -v TERMUX_VERSION ]; then
+    shell=${SHELL##*/}
+    ./start-ubuntu.sh -- $shell -c "cd $HOME; source .${shell}rc; . $HOME/.xinitrc" &
+  else
+    . ~/.xinitrc &
+  fi
 fi
