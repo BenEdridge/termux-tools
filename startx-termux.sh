087329d6d83477ac03307c2acc1cde7fed0b4222
nagasawa <nothan87@gmail.com>
2021-12-20 12:08:07 +0900

    fixed load .xinitrc

--- startx-termux.sh
+++ startx-termux.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+DIST=ubuntu
+DIST_PATH=$PREFIX/var/lib/proot-distro/installed-rootfs/$DIST
+
 termux-x11 :0 &
 
 if [ -e ~/.xinitrc ]; then
   sleep 1
-  if [`uname -o` == "Android" ]; then
-    shell=${SHELL##*/}
-    ./start-ubuntu.sh -- $shell -c "cd $HOME; source .${shell}rc; . $HOME/.xinitrc" &
+  if [ `uname -o` == "Android" ]; then
+    passwd=`grep "root" $DIST_PATH/etc/passwd`
+    shell=${passwd##*:}
+    shell_name=${shell##*/}
+    ./start-ubuntu.sh -- $shell -c "cd $HOME; source .${shell_name}rc; . $HOME/.xinitrc" &
   else
     . ~/.xinitrc &
   fi
