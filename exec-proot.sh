80ea98782ad6b8c819656dcbc67b6b62a35c211d
nagasawa <nothan87@gmail.com>
2022-01-09 15:25:17 +0900

    fixed grep passwd root -> ^root

--- exec-proot.sh
+++ exec-proot.sh
@@ -5,8 +5,13 @@ DIST=$1
 DIST_PATH=$PREFIX/var/lib/proot-distro/installed-rootfs/$DIST
 CMD=$2
 
-passwd=`grep "root" $DIST_PATH/etc/passwd`
+passwd=`grep "^root" $DIST_PATH/etc/passwd`
 shell=${passwd##*:}
 shell_name=${shell##*/}
+shell_rcfile=$HOME/.${shell_name}rc
+if [ -e $shell_rcfile ]
+then
+  CMD="source $shell_rcfile; $CMD"
+fi
 
-$SCRIPT_PATH/login-proot.sh $DIST -- $shell -c "source $HOME/.${shell_name}rc; $CMD"
+$SCRIPT_PATH/login-proot.sh $DIST -- $shell -c "$CMD"
